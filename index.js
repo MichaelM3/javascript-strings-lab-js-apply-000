8d791cd1137f4e0f9b7aa0e8d0a13c1825ed8d97
Michael Muniz <michael.muniz94@gmail.com>
2018-08-26 22:08:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var greeting = "Hello, everybody!"
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "${SpecialGuest}" + specialGuest + "!";
 
 var topic = "space";
 
